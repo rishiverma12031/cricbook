fa095935eb3d4284df083d28914f51890acf3a6f
vermarishi1203 <vermarishi1203@gmail.com>
2026-06-26 17:09:49 +0530

    add: orchestration of storage, render, and api calls

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,43 @@
 
 import { fetchPlayers, searchPlayer, lookUpPlayer } from "./api.js";
+import { transformFetchPlayers, transformLookUpPlayer } from "./transform.js";
+import { loadPlayers, savePlayers, loadModals, saveModals } from "./storage.js";
+import { renderPlayers } from "./render.js";
 
-// const players = await fetchPlayers();
-// const player = await searchPlayer("virat kohli");
-// const playerDetails = await lookUpPlayer(34170942);
+// const searchInput = document.querySelector(".search__input");
+// const clearSearchBtn = document.querySelector(".search__button--clear");
+// const searchResult = document.querySelector(".search__result");
+const IPLPlayersList = document.querySelector(".IPL-players__list");
 
-// console.log("from fetch players: ", players);
-// console.log("from search player: ", player);
-// console.log("from look up player: ", playerDetails);
+
+let players = loadPlayers();
+
+if(!players) {
+    players = transformFetchPlayers(await fetchPlayers());
+    savePlayers(players);
+}
+
+renderPlayers(players, IPLPlayersList);
+
+let modals = loadModals();
+
+IPLPlayersList.addEventListener("click", async event => {
+
+    if(!event.target.classList.contains("card")) return;
+
+    if(!modals.find(modal => modal.id === event.target.dataset.id)) {
+    
+        const playerDetails = transformLookUpPlayer(await lookUpPlayer(event.target.dataset.id));
+
+        saveModals([...modals, playerDetails]);
+
+    }
+
+
+    //call modal render function
+
+    const modal = modals.find(modal => modal.id === event.target.dataset.id);
+    
+    console.log(modal);
+
+});
