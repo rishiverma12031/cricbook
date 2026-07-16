88a1231e2b1ef2e401000c4565c3fbc06b137212
vermarishi1203 <vermarishi1203@gmail.com>
2026-07-16 12:08:31 +0530

    add: filter and sort orchestration

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,18 @@ import { renderPlayers, renderPlayer, renderSkeletonCards } from "./render.js";
 import { showModal, hideModal, showSkeletonModal, hideSkeletonModal, populateModal } from "./modal.js";
 import { debounce } from "./utils.js";
 import { showSpinner, hideSpinner } from "./spinner.js";
+import { filterByPosition, filterByNationality } from "./filter.js";
+import { sortPlayers} from "./sort.js";
 
 const searchInput = document.querySelector(".search__input");
 const clearSearchBtn = document.querySelector(".search__button--clear");
 const searchResult = document.querySelector(".search__result");
 const IPLPlayersList = document.querySelector(".IPL-players__list");
 const modalCloseBtn = document.querySelector(".modal__close");
+const filters = document.querySelector(".filters");
+const position = document.querySelector(".position");
+const nationality = document.querySelector(".nationality");
+const sort = document.querySelector(".sort");
 
 let players = loadPlayers();
 
@@ -87,3 +93,30 @@ IPLPlayersList.addEventListener("click", modalHandler);
 searchResult.addEventListener("click", modalHandler);
 
 modalCloseBtn.addEventListener("click", () => hideModal());
+
+
+let filteredPlayers = players;
+
+filters.addEventListener("change", () => {
+
+    if(!(event.target.classList.contains("position") || event.target.classList.contains("nationality"))) return;
+
+    const filteredPlayersByPostion = filterByPosition(players, position.value);
+
+    const filteredPlayersByNationality = filterByNationality(filteredPlayersByPostion, nationality.value);
+
+    filteredPlayers = [...filteredPlayersByNationality];
+
+    renderPlayers(filteredPlayers, IPLPlayersList);
+
+})
+
+sort.addEventListener("click", () => {
+
+    if(!event.target.classList.contains("button--primary")) return;
+
+    const sortedPlayers = event.target.classList.contains("sort--ascending") ? sortPlayers(filteredPlayers, "asc") : sortPlayers(filteredPlayers, "desc");
+
+    renderPlayers(sortedPlayers, IPLPlayersList);
+
+})
